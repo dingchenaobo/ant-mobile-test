038248f834d16e9e730dcd741d53a9263ef0e890
wb-dcab405553 <wb-dcab405553@alibaba-inc.com>
2018-09-10 18:19:51 +0800

    chore: change some config: 1  alias 2  lessloader  3 chrome redux-dev-tool

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,11 +1,15 @@
+declare global {
+  interface Window { __REDUX_DEVTOOLS_EXTENSION__: any; }
+}
+
 import { combineReducers, createStore, Store } from 'redux';
 
 import reducers from '../reducers';
 
-export default function configStore(initialState?: any): Store {
+export default function configStore(): Store {
   const store = createStore(
     combineReducers(reducers),
-    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   );
   return store;
 }
